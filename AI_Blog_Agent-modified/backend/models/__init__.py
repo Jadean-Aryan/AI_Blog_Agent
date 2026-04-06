81fc829c2075d24177260b4c22b9c0e90b840cf0
Jadean-Aryan <aryan.kumar@jadeglobal.com>
2026-04-06 16:48:54 +0530

    Delete AI_Blog_Agent-modified directory

--- AI_Blog_Agent-modified/backend/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .models import Base, Post, Outline, Section, SEOAnalysis, MetaTag, PostStatus, User, UserRole
-__all__ = ["Base", "Post", "Outline", "Section", "SEOAnalysis", "MetaTag", "PostStatus", "User", "UserRole"]
